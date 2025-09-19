595281042cf5b3358c8e117e4352adabda0fd99f
Amalia Bornstein <amalyb@gmail.com>
2025-09-19 13:48:29 -0700

    infra: use centralized envFlags (IS_DEV/IS_TEST) in client code

--- src/util/googleMaps.js
+++ src/util/googleMaps.js
@@ -1,8 +1,9 @@
 import { types as sdkTypes } from '../util/sdkLoader';
+import { IS_DEV } from './envFlags';
 
 const { LatLng: SDKLatLng, LatLngBounds: SDKLatLngBounds } = sdkTypes;
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = IS_DEV;
 
 /**
  * Extracts the geographic location (origin) from a Google Maps Place object
