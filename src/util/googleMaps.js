9360288ac9600c7543ca2eba1d60dcf15bdad2a3
Amalia Bornstein <amalyb@gmail.com>
2025-10-14 11:00:17 -0700

    fix(env-flags): centralize IS_DEV/IS_TEST/__DEV__ and remove duplicate declarations (fixes Render build error in googleMaps.js)

--- src/util/googleMaps.js
+++ src/util/googleMaps.js
@@ -2,7 +2,6 @@ import { types as sdkTypes } from '../util/sdkLoader';
 import { IS_DEV } from './envFlags';
 
 const { LatLng: SDKLatLng, LatLngBounds: SDKLatLngBounds } = sdkTypes;
-import { IS_DEV } from './envFlags';
 
 const isDev = IS_DEV;
 
