b4092ec730acacbc660a06c3679a364b1fa8a516
Amalia Bornstein <amalyb@gmail.com>
2025-10-23 20:23:51 -0700

    💄 UI cleanup: remove search results count from SearchPage
    - Removed `<div className={css.searchResultSummary}>` block in `MainPanelHeader.js` (lines 42–53)
    - Eliminates “X results” text from the search results page
    - Preserves all other search UI (sort dropdown, filters, grid)
    - Added comment `{/* removed results count for Sherbrt */}` for clarity

--- src/containers/SearchPage/MainPanelHeader/MainPanelHeader.js
+++ src/containers/SearchPage/MainPanelHeader/MainPanelHeader.js
@@ -39,18 +39,7 @@ const MainPanelHeader = props => {
   return (
     <div className={classes}>
       <div className={css.searchOptions}>
-        <div className={css.searchResultSummary}>
-          <span className={css.resultsFound}>
-            {searchInProgress ? (
-              <FormattedMessage id="MainPanelHeader.loadingResults" />
-            ) : (
-              <FormattedMessage
-                id="MainPanelHeader.foundResults"
-                values={{ count: resultsCount }}
-              />
-            )}
-          </span>
-        </div>
+        {/* removed results count for Sherbrt */}
         {isSortByActive ? (
           <div className={css.sortyByWrapper}>
             <span className={css.sortyBy}>
